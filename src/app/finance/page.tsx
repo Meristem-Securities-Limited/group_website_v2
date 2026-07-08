dd266f8b9b94a23550cc219b7353bf3bb2226d9e
meristemnigeria <138208339+meristemnigeria@users.noreply.github.com>
2026-07-08 18:22:11 +0100

    Update page.tsx

--- src/app/finance/page.tsx
+++ src/app/finance/page.tsx
@@ -25,9 +25,8 @@ const WealthManagementPage = () => {
         descriptions={[
           `Meristem Finance Limited is a customer-centric financial institution providing innovative, flexible, and timely financing solutions that empower businesses and individuals to achieve their growth ambitions. We specialize in structured lending, asset-backed finance, lease finance, bridge finance, LPO finance, and working capital solutions tailored to our clients' unique needs.`,
           `At Meristem Finance, we combine deep market expertise, disciplined risk management, and exceptional service to deliver responsive, value-driven financing solutions. Our relationship-led approach enables us to understand our clients' objectives and provide bespoke financial support that enhances cash flow, unlocks opportunities, and drives sustainable growth.`,
-          `Backed by the strength and credibility of the Meristem Group, we are more than a lender; we are a trusted growth partner, committed to delivering financing solutions that enable our clients scale confidently and achieve long-term success.
-          Meristem Finance Limited is registered and regulated by the Central Bank of Nigeria.
-          `,
+          `Backed by the strength and credibility of the Meristem Group, we are more than a lender; we are a trusted growth partner, committed to delivering financing solutions that enable our clients scale confidently and achieve long-term success.`
+          `Meristem Finance Limited is registered and regulated by the Central Bank of Nigeria.`,
           // `Meristem Finance Limited, the financial arm of Meristem Securities Limited, is equipped to provide financing solutions tailored to propel your business forward.
           // We're a licensed non-banking financial institution and equipment leasing firm, backed by the Central Bank of Nigeria and the Equipment Leasing Association of
           //  Nigeria (ELAN). We help you focus on your Business and equip your Growth by freeing up your capital to focus on what matters most – growing your core business.
