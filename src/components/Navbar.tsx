e238f356d649a6a2c83ec91770c2c6fb14f57e71
meristemnigeria <138208339+meristemnigeria@users.noreply.github.com>
2026-07-11 11:51:06 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -284,7 +284,7 @@ const ourProductsItems = [
         title: "Bond Investments",
         link: "/our-products/bond-investments",
       },
-      { title: "Exchange Trade Fund", link: "/our-products/exchange-trade-funds" },
+      { title: "Exchange Traded Fund", link: "/our-products/exchange-trade-funds" },
       { title: "Structured Products", link: "/our-products/structured-products" },
       { title: "Mutual Funds", link: "/our-products/mutual-funds" },
       // { title: "Nigerian Treasury Bills", link: "/our-products/nigerian-treasury-bills" },
