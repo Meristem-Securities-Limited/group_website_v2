d8088e0baa9448b37ce879f5150f83dd69c96787
ayodeji334 <fawumiayodeji@gmail.com>
2026-09-13 23:48:07 +0100

    updates

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -107,7 +107,7 @@ import Stac from "./stac.jpeg";
 import Neimeth from "./neimeth.jpeg";
 import SECLogo from "./sec-logo.svg";
 import Olusanya from "./Olusanya.jpg";
-import DangoteIpoBanner from "./dangote-ipo-banner-5.png";
+import DangoteIpoBanner from "./Now_Live jpeg.jpeg";
 
 export {
   Olusanya,
